ac49b6289e2fb1b8a1eef906b8900f18525ef0d7
NWPPLUMBING <nwpplumbing@outlook.com>
2026-06-23 16:29:06 +1000

    Fix StickyCall

--- components/StickyCall.tsx
+++ components/StickyCall.tsx
@@ -1,10 +1,7 @@
 export default function StickyCall() {
   return (
-    
-      href="tel:+61477160911"
-      className="sticky-call"
-    >
-      📞 Call Nathan on 0477 160 911
+    <a href="tel:+61477160911" className="sticky-call">
+      Call Nathan on 0477 160 911
     </a>
   )
 }
